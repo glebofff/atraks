8000d6ac71b842ee9d3fc2f238254b80788d955b
gsa <glebofff@gmail.com>
2019-11-19 14:52:06 +0500

    Initial commit

---
+++ rs_plan/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Operator(models.Model):
+    name = models.TextField(db_index=True)
+
+
+class Region(models.Model):
+    name = models.TextField(db_index=True)
+
+
+class Plan(models.Model):
+    pfx = models.CharField(max_length=3, db_index=True)
+    beg = models.CharField(max_length=7, db_index=True)
+    end = models.CharField(max_length=7, db_index=True)
+    capacity = models.SmallIntegerField(db_index=True)
+    operator = models.ForeignKey(Operator, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+
+
+
